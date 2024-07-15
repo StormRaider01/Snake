923d9e771251c6a08491cad3fdded6214f2d4ea7
Saeed Solomon <saeedsolomon786@gmail.com>
2024-07-15 15:01:50 +0200

    GamePanel update
    Declaration of private variables used in program and population of a few of the methods

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,27 +2,91 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    GamePanel() {
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;                         // x coord of apple
+    int appleY;                         // y coord of apple
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new myKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        draw(g);
     }
 
-    public void graw(Graphics g) {
+    public void draw(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
+        for (int i = 0; i < bodyParts; i++) {
+            if (i==0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+    }
+
+    public void newApple() {
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 
     public void move() {
+        for (int i = bodyParts; i>0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
 
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     public void checkApple() {
@@ -39,10 +103,15 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
-    public class MyKeyAdapter extends KeyAdapter {
+    public static class myKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
 
